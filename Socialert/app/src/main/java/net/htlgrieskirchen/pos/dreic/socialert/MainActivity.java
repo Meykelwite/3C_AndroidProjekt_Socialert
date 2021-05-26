3f45468bd7ea32b3ef089632118609b57e5c4cdc
Michael Weiß <Weiss.Michael@sus.htl-grieskirchen.at>
2021-05-26 15:46:49 +0200

    java merge conflict_Meykelwite

--- Socialert/app/src/main/java/net/htlgrieskirchen/pos/dreic/socialert/MainActivity.java
+++ Socialert/app/src/main/java/net/htlgrieskirchen/pos/dreic/socialert/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int test_Meykelwite = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
