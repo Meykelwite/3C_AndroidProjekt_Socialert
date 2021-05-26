b8fa1f8febbfdae1e50660388a6f75ea566b40db
Michael Weiß <Weiss.Michael@sus.htl-grieskirchen.at>
2021-05-26 14:47:41 +0200

    Initial commit

---
+++ Socialert/app/src/main/java/net/htlgrieskirchen/pos/dreic/socialert/MainActivity.java
@@ -0,0 +1,14 @@
+package net.htlgrieskirchen.pos.dreic.socialert;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
